373ba0b792b45014dfe90d21e90f03a83e8cb916
SugataMaji <sugatamaji94@gmail.com>
2017-07-22 23:50:56 +0530

    Typescript1 assignment2.1

---
+++ function.js
@@ -0,0 +1,9 @@
+/**
+ * Fat Arrow Function
+ * @param {string} bikeName [description]
+ */
+var details = function (Name, Age) {
+    console.log("My Name is " + Name);
+    console.log("My Age is " + Age);
+};
+details("Sugata maji", 23);
---
+++ function.ts
@@ -0,0 +1,10 @@
+/**
+ * Fat Arrow Function
+ * @param {string} bikeName [description]
+ */
+let details = (Name: string, Age: Number) => { 
+    console.log(`My Name is ${Name}`)
+    console.log(`My Age is ${Age}`)
+ }
+
+details("Sugata maji", 23);
